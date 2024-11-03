337e0bde6120e214cc55a845e536928b95472fff
KristinaDjule <89211057@student.upr.si>
2024-11-03 13:29:05 +0100

    User table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,8 +38,41 @@ function App() {
       </div>
 
 
-      <div className="items-center w-3/4 h-full bg-gray-100 p-8">
-        <h1 className="text-3xl font-bold mb-4">Syyclops Test Assignment</h1>
+      <div className="w-3/4 h-full bg-gray-100 p-8">
+        {selectedUser && (
+          <table>
+            <tbody>
+              <tr>
+                <td>ID</td>
+                <td>{selectedUser.id}</td>
+              </tr>
+              <tr>
+                <td>First Name</td>
+                <td>{selectedUser.firstName}</td>
+              </tr>
+              <tr>
+                <td>Last Name</td>
+                <td>{selectedUser.lastName}</td>
+              </tr>
+              <tr>
+                <td>Age</td>
+                <td>{selectedUser.age}</td>
+              </tr>
+              <tr>
+                <td>Gender</td>
+                <td>{selectedUser.gender}</td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td>{selectedUser.email}</td>
+              </tr>
+              <tr>
+                <td>Phone</td>
+                <td>{selectedUser.phone}</td>
+              </tr>
+            </tbody>
+          </table>
+        ) }
       </div>
     </div>
   );
