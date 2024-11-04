1769707819dea2f9a3d900e64219b8e72dfdf89f
KristinaDjule <89211057@student.upr.si>
2024-11-04 14:18:11 +0100

    Matched font family added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        syyclopsBlue: '#090f2d',
+        syyclopsOrange: 'rgb(220 90 39)'
+      },
+      fontFamily:{
+        wixDisplay: ['"Wix Madefor Display"', 'sans-serif'],
+        wixText: ['"Wix Madefor Text"', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
