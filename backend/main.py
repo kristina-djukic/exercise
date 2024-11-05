db11d7e23626edc01270b5a38ea837dd3640b56d
KristinaDjule <89211057@student.upr.si>
2024-11-05 04:13:41 +0100

    Connection to backend fixed

--- backend/main.py
+++ backend/main.py
@@ -3,32 +3,17 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from users import get_users as get_users_data
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-users = [
-    {
-        "id": 1,
-        "firstName": "John Doe",
-        "lastName": "Doe",
-        "age": 30,
-        "gender": "male",
-        "email": "john@doe.com",
-        "phone": "123-456-7890",
-    },
-    {
-        "id": 2,
-        "firstName": "Jane Doe",
-        "lastName": "Doe",
-        "age": 30,
-        "gender": "female",
-        "email": "jan@doe.com",
-        "phone": "123-456-7890",
-    },
-    # Add other users as needed
-]
-
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class userUpdate(BaseModel):
     firstName: Optional[str] = None
@@ -60,12 +45,12 @@ def get_users(skip: int = 0, limit: int = 20):      #limiting to 20 as required 
 @app.put("/users/{user_id}")
 def edit_user(user_id: int, user_update: userUpdate):
     users = get_users_data()  
-    user = next((u for u in users if u["id"] == user_id), None)
+    user = next((i for i in users if i["id"] == user_id), None)
     
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
-    for key, value in user_update.dict(exclude_unset=True).items():
+    for key, value in user_update.model_dump(exclude_unset=True).items():
         if value is not None:
             user[key] = value  
 
